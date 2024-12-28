785b134c621fcffb573941744a3477e27e4cb00b
Cyrus Wise <cyrwise@gmail.com>
2024-12-27 17:36:09 -0800

    fixed milkyway image import

--- src/components/game/MilkyWay.jsx
+++ src/components/game/MilkyWay.jsx
@@ -1,9 +1,11 @@
+// MilkyWay.jsx
 import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader, DoubleSide } from "three";
+import milkyWay from '/src/assets/images/skills/milky-way.png';
 
 const MilkyWay = () => {
-  const texture = useLoader(TextureLoader, '/src/assets/images/skills/milky-way.png');
+  const texture = useLoader(TextureLoader, milkyWay);
   return (
     <mesh rotation={[0, 0, 0]} position={[0, 0, -500]}>
       <planeGeometry args={[1000, 1000]} />
